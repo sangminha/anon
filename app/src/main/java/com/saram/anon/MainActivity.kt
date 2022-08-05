269fe526efccacc3ae2ba5ac9b17fdba7b6dba17
한상민 <ukjin2218@gmail.com>
2022-08-05 21:23:11 +0900

    login search

--- app/src/main/java/com/saram/anon/MainActivity.kt
+++ app/src/main/java/com/saram/anon/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.saram.anon
 
-import SecondActivity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -37,7 +36,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+
     private val TAG = this.javaClass.simpleName
 
     private lateinit var launcher: ActivityResultLauncher<Intent>
@@ -48,129 +47,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
 
         // 로그인 정보 확인
-         setContentView( R.layout.activity_main)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-        launcher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult(), ActivityResultCallback { result ->
-                Log.e(TAG, "resultCode : ${result.resultCode}")
-                Log.e(TAG, "result : $result")
-                if (result.resultCode == RESULT_OK) {
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                    try {
-                        task.getResult(ApiException::class.java)?.let { account ->
-                            tokenId = account.idToken
-                            if (tokenId != null && tokenId != "") {
-                                val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
-                                firebaseAuth.signInWithCredential(credential)
-                                    .addOnCompleteListener {
-                                        if (firebaseAuth.currentUser != null) {
-                                            val user: FirebaseUser = firebaseAuth.currentUser!!
-                                            email = user.email.toString()
-                                            Log.e(TAG, "email : $email")
-                                            val googleSignInToken = account.idToken ?: ""
-                                            if (googleSignInToken != "") {
-                                                Log.e(TAG, "googleSignInToken : $googleSignInToken")
-                                            } else {
-                                                Log.e(TAG, "googleSignInToken이 null")
-                                            }
-                                        }
-                                    }
-                            }
-                        } ?: throw Exception()
-                    }   catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                }
-            })
-
-        binding.run {
-            googleLoginButton.setOnClickListener {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
-                        .requestEmail()
-                        .build()
-                    val googleSignInClient = GoogleSignIn.getClient(this@MainActivity, gso)
-                    val signInIntent: Intent = googleSignInClient.signInIntent
-                    launcher.launch(signInIntent)
-
-
-        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-            if (error != null) {
-                Toast.makeText(this@MainActivity, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
-            }
-            else if (tokenInfo != null) {
-                Toast.makeText(this@MainActivity, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@MainActivity, SecondActivity::class.java)
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                finish()
-            }
-        }
-
-
-//        val keyHash = Utility.getKeyHash(this)
-//        Log.d("Hash", keyHash)
-
-
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                when {
-                    error.toString() == AccessDenied.toString() -> {
-                        Toast.makeText(this@MainActivity, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == InvalidClient.toString() -> {
-                        Toast.makeText(this@MainActivity, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == InvalidGrant.toString() -> {
-                        Toast.makeText(this@MainActivity, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == InvalidRequest.toString() -> {
-                        Toast.makeText(this@MainActivity, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == InvalidScope.toString() -> {
-                        Toast.makeText(this@MainActivity, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == Misconfigured.toString() -> {
-                        Toast.makeText(this@MainActivity, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == ServerError.toString() -> {
-                        Toast.makeText(this@MainActivity, "서버 내부 에러", Toast.LENGTH_SHORT).show()
-                    }
-                    error.toString() == Unauthorized.toString() -> {
-                        Toast.makeText(this@MainActivity, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
-                    }
-                    else -> { // Unknown
-                        Toast.makeText(this@MainActivity, "기타 에러", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-            else if (token != null) {
-                Toast.makeText(this@MainActivity, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@MainActivity, SecondActivity::class.java)
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                finish()
-            }
-        }
-
-
-        val kakao_login_button = findViewById<ImageButton>(R.id.kakao_login_button) // 로그인 버튼
-
-        kakao_login_button.setOnClickListener {
-            if(LoginClient.instance.isKakaoTalkLoginAvailable(this@MainActivity)){
-                LoginClient.instance.loginWithKakaoTalk(this@MainActivity, callback = callback)
-
-
-            }else{
-                LoginClient.instance.loginWithKakaoAccount(this@MainActivity, callback = callback)
-            }
-        }
-    }
-
-}}}}
-
-
-
+        setContentView(R.layout.activity_main)
+    }}
